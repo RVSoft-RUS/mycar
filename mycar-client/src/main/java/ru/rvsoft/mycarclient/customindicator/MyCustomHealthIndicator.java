4f17cf1a8e91dcca28a6cc02a7762a19d41b8451
rvsoft <rovs15ds@gmail.com>
2023-02-06 18:43:12 +0300

    + actuator для mycar-client + custom health indicator

---
+++ mycar-client/src/main/java/ru/rvsoft/mycarclient/customindicator/MyCustomHealthIndicator.java
@@ -0,0 +1,21 @@
+package ru.rvsoft.mycarclient.customindicator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
+public class MyCustomHealthIndicator implements HealthIndicator {
+    private final ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
+
+    @Override
+    public Health health() {
+        int code = threadLocalRandom.nextInt(2);
+        if (code != 0) {
+            return Health.down().withDetail("Error Code", code).build();
+        }
+        return Health.up().build();
+    }
+}
